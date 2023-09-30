279e2cb5aa902606a0cc0dc9bf2e984190e7c4e0
AndrewIndeche <indecheandrew@outlook.com>
2023-09-30 23:10:15 +0300

    add new django project

--- address_management/address_management/wsgi.py
+++ address_management/address_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AddressManagementTool project.
+WSGI config for address_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AddressManagementTool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'address_management.settings')
 
 application = get_wsgi_application()
