44084d52a3764f7899815be9896e74cc7f303e65
AndrewIndeche <indecheandrew@outlook.com>
2023-10-02 01:12:15 +0300

    revert to sqlite

--- address_management/settings.py
+++ address_management/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'address_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-        "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT'),
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
