14c8058ca34d8ccdeae2b0b1ecf96e10b088cde7
AndrewIndeche <indecheandrew@outlook.com>
2023-10-02 12:26:22 +0300

    add DB variables to .env

--- address_management/settings.py
+++ address_management/settings.py
@@ -82,11 +82,12 @@ WSGI_APPLICATION = 'address_management.wsgi.application'
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'IPMGTTOOL',
-        'USER': 'andrew',
-        'PASSWORD': 'Omwami',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('PORT')
+
     }
 }
 
