e597102ac8a21ada3c3382e3aed1638e3fec348a
AndrewIndeche <indecheandrew@outlook.com>
2023-10-04 19:22:12 +0300

    update tests and README

--- tool/views_tests.py
+++ tool/views_tests.py
@@ -1,161 +1,66 @@
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
-from .models import Address,Customer
+# test.py
+from django.test import TestCase, Client
 import pytest
-from django.urls import reverse
-import ipaddress
-
+from django.contrib.auth.models import User
+from .models import Address, Customer
 
 @pytest.mark.django_db
-class IPAllocationViewTest(TestCase):
+class ViewTest(TestCase):
     def setUp(self):
-        # Create some IP addresses and customers
-        self.ip1 = Address.objects.create(ip="192.168.1.10")
-        self.ip2 = Address.objects.create(ip="192.168.1.11", customer="Alice", allocated=True)
-        self.user = User.objects.create_user(username="testuser", password="testpass")
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.customer = Customer.objects.create(name='testcustomer', email='test@test.com')
+        self.ip1 = Address.objects.create(ip='192.168.0.1', allocated=False)
+        self.ip2 = Address.objects.create(ip='192.168.0.2', allocated=True, customer=self.customer)
 
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_ip_allocation_post_success(self):
-        # Test the IP allocation view with a valid POST request
-        self.client.login(username="testuser", password="testpass")
-        data = {"name": "Bob", "email": "bob@example.com"}
-        response = self.client.post(reverse("ip_allocation", args=[self.ip1.ip]), data=data)
+    def test_ip_allocation(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post('/ip_allocation/192.168.0.1/', {'name': 'newcustomer', 'email': 'new@test.com'})
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json(), {"ip": self.ip1.ip, "customer": "Bob", "email": "bob@example.com"})
-        self.ip1.refresh_from_db()
-        self.assertEqual(self.ip1.customer, "Bob")
-        self.assertEqual(self.ip1.allocated, True)
+        self.assertEqual(response.json(), {'ip': '192.168.0.1', 'customer': 'newcustomer', 'email': 'new@test.com'})
+        self.assertTrue(Address.objects.get(ip='192.168.0.1').allocated)
+        self.assertEqual(Address.objects.get(ip='192.168.0.1').customer.name, 'newcustomer')
     
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_ip_allocation_post_failure(self):
-        # Test the IP allocation view with an invalid POST request
-        self.client.login(username="testuser", password="testpass")
-        data = {"name": "", "email": ""}
-        response = self.client.post(reverse("ip_allocation", args=[self.ip1.ip]), data=data)
+    def test_ip_allocation_invalid_data(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post('/ip_allocation/192.168.0.1/', {'name': '', 'email': ''})
         self.assertEqual(response.status_code, 400)
-        self.ip1.refresh_from_db()
-        self.assertIsNone(self.ip1.customer)
-        self.assertEqual(self.ip1.allocated, False)
     
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_ip_allocation_post_no_ip(self):
-        # Test the IP allocation view with a non-existing IP address
-        self.client.login(username="testuser", password="testpass")
-        data = {"name": "Bob", "email": "bob@example.com"}
-        response = self.client.post(reverse("ip_allocation", args=["192.168.1.12"]), data=data)
+    def test_ip_allocation_no_ip_available(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post('/ip_allocation/192.168.0.3/', {'name': 'newcustomer', 'email': 'new@test.com'})
         self.assertEqual(response.status_code, 500)
-        self.assertEqual(response.content.decode(), "No IPs are available")
+        self.assertEqual(response.content, b'No IPs are available')
     
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_ip_allocation_get_not_allowed(self):
-        # Test the IP allocation view with a GET request
-        self.client.login(username="testuser", password="testpass")
-        response = self.client.get(reverse("ip_allocation", args=[self.ip1.ip]))
-        self.assertEqual(response.status_code, 405)
-
-@pytest.mark.django_db
-class ReleaseIPViewTest(TestCase):
-    def setUp(self):
-        # Create some IP addresses and customers
-        self.ip1 = Address.objects.create(ip="192.168.1.10")
-        self.ip2 = Address.objects.create(ip="192.168.1.11", customer="Alice", allocated=True)
-        self.user = User.objects.create_user(username="testuser", password="testpass")
-    
-    @pytest.mark.django_db
-    @pytest.fixture
-    def test_release_ip_put_success(self):
-         # Test the release IP view with a valid PUT request
-         self.client.login(username="testuser", password="testpass")
-         response = self.client.put(reverse("release_ip", args=[self.ip2.ip]))
-         self.assertEqual(response.status_code, 200)
-         self.assertEqual(response.content.decode(), "successful release ")
-         self.ip2.refresh_from_db()
-         self.assertIsNone(self.ip2.customer)
-         self.assertEqual(self.ip2.allocated, False)
-    
-    @pytest.mark.django_db
-    @pytest.fixture
-    def test_release_ip_put_failure(self):
-         # Test the release IP view with an invalid PUT request
-         self.client.login(username="testuser", password="testpass")
-         response = self.client.put(reverse("release_ip", args=[self.ip1.ip]))
-         self.assertEqual(response.status_code, 404)
-         self.assertEqual(response.content.decode(), "no IP has been allocated")
-         self.ip1.refresh_from_db()
-         self.assertIsNone(self.ip1.customer)
-         self.assertEqual(self.ip1.allocated, False)
-    
-    @pytest.mark.django_db
-    @pytest.fixture
-    def test_release_ip_put_no_ip(self):
-         # Test the release IP view with a non-existing IP address
-         self.client.login(username="testuser", password="testpass")
-         response = self.client.put(reverse("release_ip", args=["192.168.1.12"]))
-         self.assertEqual(response.status_code, 404)
-         self.assertEqual(response.content.decode(), "No IP has been found")
-    
-    @pytest.mark.django_db
-    @pytest.fixture
-    def test_release_ip_get_not_allowed(self):
-         # Test the release IP view with a GET request
-         self.client.login(username="testuser", password="testpass")
-         response = self.client.get(reverse("release_ip", args=[self.ip2.ip]))
-         self.assertEqual(response.status_code, 405)
-
-@pytest.mark.django_db
-class AllocatedIPsViewTest(TestCase):
-    def setUp(self):
-        # Create some IP addresses and customers
-        Address.objects.create(ip="192.168.1.10")
-        Address.objects.create(ip="192.168.1.11", customer="Alice", allocated=True)
-        Address.objects.create(ip="192.168.1.12", customer="Bob", allocated=True)
-        self.user = User.objects.create_user(username="testuser", password="testpass")
-    
-    @pytest.mark.django_db
-    @pytest.fixture
-    def test_allocated_ips_get_success(self):
-        # Test the allocated IPs view with a valid GET request
-        self.client.login(username="testuser", password="testpass")
-        response = self.client.get(reverse("allocated_ips"))
+    def test_release_ip(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.put('/release_ip/192.168.0.2/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), [
-            {"ip": "192.168.1.11", "customer__name": "Alice"},
-            {"ip": "192.168.1.12", "customer__name": "Bob"}
-        ])
+        self.assertEqual(response.content, b'successful release ')
+        self.assertFalse(Address.objects.get(ip='192.168.0.2').allocated)
+        self.assertIsNone(Address.objects.get(ip='192.168.0.2').customer)
+    
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_allocated_ips_post_not_allowed(self):
-        # Test the allocated IPs view with a POST request
-        self.client.login(username="testuser", password="testpass")
-        response = self.client.post(reverse("allocated_ips"))
-        self.assertEqual(response.status_code, 405)
-
-@pytest.mark.django_db
-class AvailableIPsViewTest(TestCase):
-    def setUp(self):
-        # Create some IP addresses and customers
-        Address.objects.create(ip="192.168.1.10")
-        Address.objects.create(ip="192.168.1.11", customer="Alice", allocated=True)
-        Address.objects.create(ip="192.168.1.12")
-        self.user = User.objects.create_user(username="testuser", password="testpass")
+    def test_release_ip_no_ip_allocated(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.put('/release_ip/192.168.0.1/')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content, b'no IP has been allocated')
+    
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_available_ips_get_success(self):
-         # Test the available IPs view with a valid GET request
-         self.client.login(username="testuser", password="testpass")
-         response = self.client.get(reverse("available_ips"))
-         self.assertEqual(response.status_code, 200)
-         self.assertEqual(response.json(), ["192.168.1.10", "192.168.1.12"])
-
+    def test_allocated_ips(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get('/allocated_ips/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [{'ip': '192.168.0.2', 'customer__name': 'testcustomer', 'email': 'test@test.com'}])
+    
     @pytest.mark.django_db
-    @pytest.fixture
-    def test_available_ips_post_not_allowed(self):
-         # Test the available IPs view with a POST request
-         self.client.login(username="testuser", password="testpass")
-         response = self.client.post(reverse("available_ips"))
-         self.assertEqual(response.status_code, 405)
+    def test_available_ips(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get('/available_ips/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), ['192.168.0.1'])
