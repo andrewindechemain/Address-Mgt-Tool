e32ab7fa668ebe01a401b90e7203273593d5f256
AndrewIndeche <indecheandrew@outlook.com>
2023-10-05 14:12:25 +0300

    edit logic for views

--- tool/views.py
+++ tool/views.py
@@ -10,13 +10,13 @@ class IpAllocationView(APIView):
     @login_required
     def post(self, request, Address):
         data = json.loads(request.body)
-        ip = Address.objects.get(ip=Address.IPv4Address(ip))
+        ip = Address.objects.get('ip','customer', 'allocated')
 
         if not data.get("name") or not data.get("email"):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         if ip:
             ip.customer = Address.objects.name
-            ip.allocated = True
+            ip.allocated = Tr
             ip.save()
             return Response({'ip': ip.ip, 'customer': ip.customer.name, 'email'
                              : ip.customer.email}, status=status.HTTP_201_CREATED)
@@ -24,13 +24,13 @@ class IpAllocationView(APIView):
 
 class ReleaseIpView(APIView):
     @login_required
-    def put(self, request, Address):
-        ip = Address.objects.get(ip=Address.IPv4Address(ip))
+    def put(self,Address):
+        ip = Address.objects.get('ip', 'customer', 'allocated')
         if ip.allocated:
             ip.customer = None
             ip.allocated = False
             ip.save()
-            return Response('successful release ', status=status.HTTP_200_OK)
+            return Response('successful released', list(ip.allocated) , status=status.HTTP_200_OK)
         return Response('no IP has been allocated', status=status.HTTP_404_NOT_FOUND)
 
 class AllocatedIpsView(APIView):
