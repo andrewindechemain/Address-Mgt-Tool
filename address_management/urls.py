90f46bec5adb563baf93af3b54601717f668fb89
AndrewIndeche <indecheandrew@outlook.com>
2023-10-05 10:57:09 +0300

    update urls

--- address_management/urls.py
+++ address_management/urls.py
@@ -35,10 +35,11 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('rest_framework.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
     path('tool/', include('tool.urls')),
     path('accounts/', include("tool.urls")),
 ]
